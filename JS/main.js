342835c2131e6fc47464a3ba9382ce94d1fd0752
Bashar <yaser07739033141@gmail.com>
2022-12-29 14:09:24 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+document.querySelector('.btn').addEventListener('click', (evt) => {
+    evt.target.classList.add('loading')
+
+    setTimeout(() => {
+    evt.target.classList.remove('loading')
+}, 3000);
+})
+
+
+
